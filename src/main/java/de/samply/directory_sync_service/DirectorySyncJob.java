0c402ed151033b259d78bc00aea0e35d2eb123f0
Croft <d506t@ad.dkfz-heidelberg.de>
2023-01-30 15:32:18 +0100

    Merged DirectorySyncLauncher into DirectorySyncJobScheduler
    ...to avoid having redundant classes.

--- src/main/java/de/samply/directory_sync_service/DirectorySyncJob.java
+++ src/main/java/de/samply/directory_sync_service/DirectorySyncJob.java
@@ -1,5 +1,7 @@
 package de.samply.directory_sync_service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
@@ -11,6 +13,8 @@ import org.quartz.JobExecutionException;
  * Can handle single-run or repeated run operations.
  */
 public class DirectorySyncJob implements Job {
+    private static Logger logger = LogManager.getLogger(DirectorySyncLauncher.class);
+
     /**
      * Method used by Quartz to start a job.
      *
@@ -34,5 +38,22 @@ public class DirectorySyncJob implements Job {
      */
     public void execute(Configuration configuration) throws JobExecutionException {
         new DirectorySync().syncWithDirectoryFailover(configuration);
+    }
+
+    /**
+     * Test to see if the preconditions for executing a job are available.
+     *
+     * @param configuration Spring Boot configuration for this service.
+     * @return True, if the preconditions are satidfied, false otherwise.
+     */
+    public boolean isExecutable(Configuration configuration) {
+        String directoryUserName = configuration.getDirectoryUserName();
+        String directoryUserPass = configuration.getDirectoryUserPass();
+        if (directoryUserName == null || directoryUserName.isEmpty() || directoryUserPass == null || directoryUserPass.isEmpty()) {
+            logger.warn("Directory user name or pass code is empty, will *not* perform Directory sync");
+            return false;
+        }
+
+        return true;
     }
 }
--- src/main/java/de/samply/directory_sync_service/DirectorySyncLauncher.java
+++ src/main/java/de/samply/directory_sync_service/DirectorySyncLauncher.java
@@ -2,6 +2,16 @@ package de.samply.directory_sync_service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,23 +19,53 @@ import org.springframework.stereotype.Service;
 @Service
 public class DirectorySyncLauncher {
   private static Logger logger = LogManager.getLogger(DirectorySyncLauncher.class);
+  private static final String jobType = "directorySync"; // The type of the job. This is used primarily in diagnostics.
 
   @Autowired
   Configuration configuration;
 
-  /** Starts the transformation. */
   public void run() throws Exception {
-    logger.info("Starting Directory sync");
-
-    String directoryUserName = configuration.getDirectoryUserName();
-    String directoryUserPass = configuration.getDirectoryUserPass();
-    if (directoryUserName == null || directoryUserName.isEmpty() || directoryUserPass == null || directoryUserPass.isEmpty()) {
-      logger.warn("Directory user name or pass code is empty, will *not* perform Directory sync");
+    if (!(new DirectorySyncJob().isExecutable(configuration)))
       return;
-    }
 
     String timerCron = configuration.getTimerCron();
 
-    new DirectorySyncJobScheduler("directorySync").jobStart(timerCron, configuration);
+    if (timerCron == null || timerCron.isEmpty()) {
+      logger.info("Running job just once");
+      try {
+        (new DirectorySyncJob()).execute(configuration);
+      } catch (JobExecutionException e) {
+        e.printStackTrace();
+      }
+      return;
+    }
+
+    // Quartz likes to have a " ?" at the end of its cron definition.
+    if (!timerCron.endsWith(" ?"))
+      timerCron = timerCron + " ?";
+
+    logger.info("Running job repeatedly, according to following cron schedule: " + timerCron);
+    JobKey quartzJobKey = new JobKey(jobType + "Job", jobType);
+    JobDetail quartzJob = JobBuilder.newJob(DirectorySyncJob.class)
+            .withIdentity(quartzJobKey).build();
+
+    // Pass configuration to job
+    quartzJob.getJobDataMap().put("configuration", configuration);
+
+    Trigger quartzTrigger = TriggerBuilder
+            .newTrigger()
+            .withIdentity(jobType + "Trigger", jobType)
+            .withSchedule(
+                    CronScheduleBuilder.cronSchedule(timerCron))
+            .build();
+
+    try {
+      Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+
+      scheduler.start();
+      scheduler.scheduleJob(quartzJob, quartzTrigger);
+    } catch (SchedulerException e) {
+      logger.error(e.toString());
+    }
   }
 }
