fd7fe69e476d7c3d08c9ea60d7eea1cf8d50b027
David Croft <d.croft@dkfz-heidelberg.de>
2025-04-10 14:14:49 +0200

    Added tests for the principal Directory update processes

---
+++ src/test/java/de/samply/directory_sync_service/sync/CollectionUpdaterTest.java
@@ -0,0 +1,81 @@
+package de.samply.directory_sync_service.sync;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import de.samply.directory_sync_service.directory.DirectoryApiWriteToFile;
+import de.samply.directory_sync_service.fhir.model.FhirCollection;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CollectionUpdaterTest {
+    @Test
+    public void testSendStarModelUpdatesToDirectory() {
+        // Use DirectoryApiWriteToFile because it caches intermediate results
+        // and we can use these for checking the operation of the method being tested.
+        DirectoryApiWriteToFile directoryApi = new DirectoryApiWriteToFile(null);
+
+        // Synthetic input data for the method being tested.
+        String fhirCollectionsJsonString = "[\n" +
+                "  {\n" +
+                "    \"id\": \"bbmri-eric:ID:EU_BBMRI-ERIC:collection:CRC-Cohort\",\n" +
+                "    \"size\": 1,\n" +
+                "    \"numberOfDonors\": 1,\n" +
+                "    \"sex\": [\n" +
+                "      \"female\"\n" +
+                "    ],\n" +
+                "    \"ageLow\": 42,\n" +
+                "    \"ageHigh\": 42,\n" +
+                "    \"materials\": [\n" +
+                "      \"TISSUE_PARAFFIN_EMBEDDED\"\n" +
+                "    ],\n" +
+                "    \"storageTemperatures\": [],\n" +
+                "    \"diagnosisAvailable\": [\n" +
+                "      \"C18.0\"\n" +
+                "    ]\n" +
+                "  }\n" +
+                "]";
+        Gson gson = new Gson();
+        List<FhirCollection> fhirCollections = gson.fromJson(fhirCollectionsJsonString, new TypeToken<List<FhirCollection>>() {}.getType());
+        Map<String, String> correctedDiagnoses = null;
+
+        // Call the method
+        boolean result = CollectionUpdater.sendUpdatesToDirectory(directoryApi, correctedDiagnoses, fhirCollections);
+
+        // Assert expected outcomes
+        assertTrue(result, "Method should return true if the updates were successful");
+
+        // Get the results from the DirectoryApiWriteToFile object.
+        String entityTableString = directoryApi.getEntityTableString();
+        assertNotNull(entityTableString, "factTableString should not be null");
+
+        // Assert that the fact table has the expected number of lines and columns
+        String[] entityTableLines = entityTableString.split("\n");
+        assertEquals(2, entityTableLines.length, "DirectoryApiWriteToFile should write 2 lines to the entity table");
+        String[] headerRowColumns = entityTableLines[0].split(";");
+        assertEquals(16, headerRowColumns.length, "DirectoryApiWriteToFile should write 16 columns in the header row");
+        String[] dataRowColumns = entityTableLines[1].split(";");
+        assertEquals(16, dataRowColumns.length, "DirectoryApiWriteToFile should write 16 columns in the data row");
+
+        // Assert that the data in the fact table is as expected
+        assertEquals("bbmri-eric:ID:EU_BBMRI-ERIC:collection:CRC-Cohort", dataRowColumns[0], "The first column in the data row should be the collection ID");
+        assertEquals("EU", dataRowColumns[1], "The second column in the data row should be country EU");
+        assertEquals("SAMPLE", dataRowColumns[2], "The third column in the data row should be type SAMPLE");
+        assertEquals("BIOLOGICAL_SAMPLES", dataRowColumns[3], "The fourth column in the data row should be data category BIOLOGICAL_SAMPLES");
+        assertEquals("0", dataRowColumns[4], "The fifth column in the data row should be order of magnitude 0");
+        assertEquals("1", dataRowColumns[5], "The sixth column in the data row should be size 1");
+        assertEquals("1", dataRowColumns[7], "The eigth column in the data row should be number of donors 1");
+        assertEquals("0", dataRowColumns[8], "The ninth column in the data row should be the order of magnitude donors 0");
+        assertEquals("FEMALE", dataRowColumns[9], "The tenth column in the data row should be the sex FEMALE");
+        assertEquals("urn:miriam:icd:C18.0", dataRowColumns[10], "The eleventh column in the data row should be the diagnosis urn:miriam:icd:C18.0");
+        assertEquals("42", dataRowColumns[11], "The twelfth column in the data row should be the age low 42");
+        assertEquals("42", dataRowColumns[12], "The thirteenth column in the data row should be the age high 42");
+        assertEquals("TISSUE_PARAFFIN_EMBEDDED", dataRowColumns[13], "The fourteenth column in the data row should be the materials TISSUE_PARAFFIN_EMBEDDED");
+        assertEquals("EU", dataRowColumns[15], "The sixteenth column in the data row should be the national node EU");
+    }
+}
---
+++ src/test/java/de/samply/directory_sync_service/sync/StarModelUpdaterTest.java
@@ -0,0 +1,71 @@
+package de.samply.directory_sync_service.sync;
+
+import com.google.gson.Gson;
+import de.samply.directory_sync_service.directory.DirectoryApiWriteToFile;
+import de.samply.directory_sync_service.model.StarModelData;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Map;
+
+public class StarModelUpdaterTest {
+    @Test
+    public void testSendStarModelUpdatesToDirectory() {
+        // Use DirectoryApiWriteToFile because it caches intermediate results
+        // and we can use these for checking the operation of the method being tested.
+        DirectoryApiWriteToFile directoryApi = new DirectoryApiWriteToFile(null);
+
+        // Synthetic input data for the method being tested.
+        String starModelInputDataJsonString = "{\n" +
+                "  \"minDonors\": 10,\n" +
+                "  \"ageAtPrimaryDiagnosisWarningCounter\": 0,\n" +
+                "  \"inputData\": {\n" +
+                "    \"bbmri-eric:ID:EU_BBMRI-ERIC:collection:CRC-Cohort\": [\n" +
+                "      {\n" +
+                "        \"sample_material\": \"TISSUE_PARAFFIN_EMBEDDED\",\n" +
+                "        \"hist_loc\": \"urn:miriam:icd:C18.0\",\n" +
+                "        \"sex\": \"FEMALE\",\n" +
+                "        \"age_at_primary_diagnosis\": \"35\",\n" +
+                "        \"collection\": \"bbmri-eric:ID:EU_BBMRI-ERIC:collection:CRC-Cohort\",\n" +
+                "        \"id\": \"70566273-0951-45e1-91a7-be8387f8edd0\"\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"factTables\": []\n" +
+                "}";
+        Gson gson = new Gson();
+        StarModelData starModelInputData = gson.fromJson(starModelInputDataJsonString, StarModelData.class);
+        Map<String, String> correctedDiagnoses = null;
+        int minDonors = 0;
+        int maxFacts = (-1);
+
+        // Call the method
+        boolean result = StarModelUpdater.sendStarModelUpdatesToDirectory(directoryApi, correctedDiagnoses, starModelInputData, minDonors, maxFacts);
+
+        // Assert expected outcomes
+        assertTrue(result, "Method should return true if the updates were successful");
+
+        // Get the results from the DirectoryApiWriteToFile object.
+        String factTableString = directoryApi.getFactTableString();
+        assertNotNull(factTableString, "factTableString should not be null");
+
+        // Assert that the fact table has the expected number of lines and columns
+        String[] factTableLines = factTableString.split("\n");
+        assertEquals(2, factTableLines.length, "DirectoryApiWriteToFile should write 2 lines to the fact table");
+        String[] headerRowColumns = factTableLines[0].split(";");
+        assertEquals(10, headerRowColumns.length, "DirectoryApiWriteToFile should write 10 columns in the header row");
+        String[] dataRowColumns = factTableLines[1].split(";");
+        assertEquals(10, dataRowColumns.length, "DirectoryApiWriteToFile should write 10 columns in the data row");
+
+        // Assert that the data in the fact table is as expected
+        assertEquals("bbmri-eric:factID:EU_BBMRI-ERIC:collection:CRC-Cohort:663481554", dataRowColumns[0], "The first column in the data row should be the fact ID");
+        assertEquals("FEMALE", dataRowColumns[1], "The second column in the data row should be sex FEMALE");
+        assertEquals("urn:miriam:icd:C18.0", dataRowColumns[2], "The third column in the data row should be disease urn:miriam:icd:C18.0");
+        assertEquals("Adult", dataRowColumns[3], "The fourth column in the data row should be age Adult");
+        assertEquals("TISSUE_PARAFFIN_EMBEDDED", dataRowColumns[4], "The fifth column in the data row should be sample type TISSUE_PARAFFIN_EMBEDDED");
+        assertEquals("1", dataRowColumns[5], "The sixth column in the data row should be number of donors 1");
+        assertEquals("1", dataRowColumns[6], "The seventh column in the data row should be number of samples 1");
+        assertEquals("bbmri-eric:ID:EU_BBMRI-ERIC:collection:CRC-Cohort", dataRowColumns[8], "The ninth column in the data row should be the collection ID");
+        assertEquals("EU", dataRowColumns[9], "The tenth column in the data row should be the national node EU");
+    }
+}
