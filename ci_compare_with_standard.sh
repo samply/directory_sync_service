c98b9f10892a1e74deb9ee3adc24b883673d41b1
David Croft <d.croft@dkfz-heidelberg.de>
2025-04-22 12:22:03 +0200

    Added collections to the comparison

--- ci_compare_with_standard.sh
+++ ci_compare_with_standard.sh
@@ -1,5 +1,5 @@
+# Preprocess standard fact table data
 cat data/crc-cohort-facts-full.csv | \
-#cat data/crc-cohort-facts-1patient.csv | \
 # Remove column 8 (last_update), since this is is never the same.
 awk -F';' 'BEGIN {OFS=";"} {for(i=1;i<=NF;i++) if(i!=8) printf "%s%s", $i, (i<NF ? OFS : ""); printf "\n"}' | \
 # Correct ICD code
@@ -11,8 +11,8 @@ sed 's/Cohort[^;]*;/Cohort;/' | \
 # Remove header row
 grep -v "sample_type;number_of_donors;number_of_samples" | \
 sort > crc-cohort_facts.processed.csv
-#sort | uniq > crc-cohort_facts.processed.csv
 
+# Preprocess generated output fact table data
 cat test/DirectoryFactTables.csv | \
 # Remove column 10 (national_node), which is not in Petr's fact tables.
 awk -F';' 'BEGIN {OFS=";"} {for(i=1;i<=NF;i++) if(i!=10) printf "%s%s", $i, (i<NF ? OFS : ""); printf "\n"}' | \
@@ -26,12 +26,36 @@ sed 's/_collection_/:collection:/' | \
 # Remove header row
 grep -v "sample_type;number_of_donors;number_of_samples" | \
 sort > DirectoryFactTables.processed.csv
-#sort | uniq > DirectoryFactTables.processed.csv
 
+# Compare fact tables
 if diff crc-cohort_facts.processed.csv DirectoryFactTables.processed.csv; then
-    echo "Output matches against standard."
+    echo "Fact table output matches against standard."
 else
-    echo "Output does not match against standard."
+    echo "Fact table output does not match against standard."
+    exit 1
+fi
+
+# Preprocess standard fact table data
+cat data/crc-cohort-collections-full.csv | \
+# Remove column 7 (last_update), since this is is never the same.
+awk -F';' 'BEGIN {OFS=";"} {for(i=1;i<=NF;i++) if(i!=7) printf "%s%s", $i, (i<NF ? OFS : ""); printf "\n"}' | \
+# Remove header row
+grep -v "order_of_magnitude" | \
+sort > crc-cohort_collections.processed.csv
+
+# Preprocess generated output fact table data
+cat test/DirectoryCollections.csv | \
+# Remove column 7 (last_update), since this is is never the same.
+awk -F';' 'BEGIN {OFS=";"} {for(i=1;i<=NF;i++) if(i!=7) printf "%s%s", $i, (i<NF ? OFS : ""); printf "\n"}' | \
+# Remove header row
+grep -v "order_of_magnitude" | \
+sort > DirectoryCollections.processed.csv
+
+# Compare collections
+if diff crc-cohort_collections.processed.csv DirectoryCollections.processed.csv; then
+    echo "Collections output matches against standard."
+else
+    echo "Collections output does not match against standard."
     exit 1
 fi
 
