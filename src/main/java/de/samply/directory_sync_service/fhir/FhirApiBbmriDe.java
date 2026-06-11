7b1a57d6871215865b2d9d249c641660fb581c80
DavidCroftDKFZ <46788708+DavidCroftDKFZ@users.noreply.github.com>
2026-06-11 12:39:01 +0200

    Moved some methods back into FhirApi
    I am going through the methods in FhirApiBbmriDe to see which are either
    immediately suitable for both data models, or which can be generalized
    to make them so.

--- src/main/java/de/samply/directory_sync_service/fhir/FhirApiBbmriDe.java
+++ src/main/java/de/samply/directory_sync_service/fhir/FhirApiBbmriDe.java
@@ -56,228 +56,22 @@ import static org.hl7.fhir.r4.model.OperationOutcome.IssueSeverity.ERROR;
  */
 public class FhirApiBbmriDe extends FhirApi {
   protected static final Logger logger = LoggerFactory.getLogger(FhirApiBbmriDe.class);
-  protected static final String MIABIS_BIOBANK_PROFILE_URI = "https://fhir.bbmri-eric.eu/StructureDefinition/miabis-biobank"; // Needed by isMiabisOnFhirProfile
   protected static final String STORAGE_TEMPERATURE_URI = "https://fhir.bbmri.de/StructureDefinition/StorageTemperature";
   protected static final String SAMPLE_DIAGNOSIS_URI = "https://fhir.bbmri.de/StructureDefinition/SampleDiagnosis";
   protected static final String BIOBANK_PROFILE_URI = "https://fhir.bbmri.de/StructureDefinition/Biobank";
   protected static final String COLLECTION_PROFILE_URI = "https://fhir.bbmri.de/StructureDefinition/Collection";
-  protected static final String DEFAULT_COLLECTION_ID = "DEFAULT_1010101";
 
   public FhirApiBbmriDe(String fhirStoreUrl) {
     super(fhirStoreUrl);
     logger.debug("FhirApiBbmriDe: initialized");
   }
 
-  /**
-   * Returns the BBMRI-ERIC identifier of {@code collection} if some valid one could be found.
-   *
-   * @param collection the Organization resource, possibly containing a BBMRI-ERIC identifier
-   * @return the found BBMRI-ERIC identifier or {@link Optional#empty empty}
-   */
-  public static Optional<BbmriEricId> bbmriEricId(Organization collection) {
-    return collection.getIdentifier().stream()
-        .filter(i -> "http://www.bbmri-eric.eu/".equals(i.getSystem()))
-        .findFirst().map(Identifier::getValue).flatMap(BbmriEricId::valueOf);
+  protected String getBiobankProfileUri() {
+    return BIOBANK_PROFILE_URI;
   }
 
-  /**
-   * Updates the provided resource on the FHIR server.
-   * <p>
-   * This method updates the specified resource on the FHIR server and returns the operation outcome.
-   *
-   * @param resource The resource to be updated on the FHIR server.
-   * @return The operation outcome of the update operation.
-   *         If the update is successful, the operation outcome will contain information about the update.
-   *         If an exception occurs during the update process, an error operation outcome will be returned.
-   */
-  public OperationOutcome updateResource(IBaseResource resource) {
-    logger.debug("updateResource: entered");
-
-    // Remove the version ID, so that no If-Match header gets added to the request
-    // If you don't do this, Blaze will throw an exception like this:
-    // "Precondition `\"1\"` failed on `Organization/biobank-0`.", :cognitect.anomalies/category :cognitect.anomalies/conflict, :http/status 412}
-    // According to Alex: . Diese Fehlermeldung kommt, wenn Du entweder beim Laden einen If-Match header mitschickst oder im Request Teil vom Transaction Bundle eine ifMatch Property angibst
-    resource.setId(resource.getIdElement().toUnqualifiedVersionless());
-
-    try {
-      IBaseOperationOutcome outcome = fhirClient
-              .update()
-              .resource(resource)
-              .prefer(OPERATION_OUTCOME)
-              .execute()
-              .getOperationOutcome();
-      logger.debug("updateResource: return outcome: " +outcome);
-      return (OperationOutcome) outcome;
-    } catch (Exception e) {
-      logger.warn("updateResource: exception: " + Util.traceFromException(e));
-      OperationOutcome outcome = new OperationOutcome();
-      outcome.addIssue().setSeverity(ERROR).setDiagnostics(e.getMessage());
-      return outcome;
-    }
-  }
-
-  /**
-   * Lists all Organization resources with the biobank profile.
-   *
-   * @return a list of {@link Organization} resources or null on *
-   * errors
-   */
-  public List<Organization> listAllBiobanks() {
-    // List all organizations with the specified biobank profile URI
-    Bundle organizationBundle = listAllOrganizations(BIOBANK_PROFILE_URI);
-
-    // Check if the operation was successful
-    if (organizationBundle == null) {
-      logger.warn("listAllBiobanks: there was a problem during listAllOrganizations");
-      return null;
-    }
-
-    // Extract the organizations from the Bundle and return them
-    return extractOrganizations(organizationBundle, BIOBANK_PROFILE_URI);
-  }
-
-  /**
-   * Retrieves a list of all known collections.
-   *
-   * @return A list of {@link Organization} objects representing biobank collections,
-   *         or {@code null} if an error occurs.
-   */
-  protected List<Organization> listAllCollections() {
-    // List all organizations with the specified biobank profile URI
-    Bundle organizationBundle = listAllOrganizations(COLLECTION_PROFILE_URI);
-
-    // Check if the operation was successful
-    if (organizationBundle == null) {
-      logger.warn("listAllCollections: there was a problem during listAllOrganizations");
-      return null;
-    }
-
-    // Extract the organizations from the Bundle and return them
-    return extractOrganizations(organizationBundle, COLLECTION_PROFILE_URI);
-  }
-
-  /**
-   * Retrieves a list of all collections that match the given list of collection IDs.
-   *
-   * @param collectionIds A list of collection IDs.
-   * @return A list of {@link Organization} objects representing biobank collections,
-   *         or {@code null} if an error occurs.
-   */
-  public List<Organization> listAllCollections(List<String> collectionIds) {
-    List<Organization> collections = listAllCollections();
-    if (collections == null) {
-      logger.warn("listAllCollections: collections is null");
-      return null;
-    }
-
-    return collections.stream()
-            .filter(c -> c.getIdentifier().stream()
-                    .anyMatch(identifier -> collectionIds.contains(identifier.getValue())))
-            .collect(Collectors.toList());
-  }
-
-    /**
-     * Retrieves all organizations that conform to a given FHIR profile URI.
-     *
-     * <p>This method performs a FHIR search query to find organizations matching the provided profile URI.
-     * If an exception occurs during the search, it logs the error and returns {@code null}.
-     *
-     * @param profileUri The URI of the FHIR profile used to filter the organizations.
-     * @return A {@link Bundle} containing the matching organizations, or {@code null} if an error occurs.
-     */
-  protected Bundle listAllOrganizations(String profileUri) {
-    try {
-      return (Bundle) fhirClient.search().forResource(Organization.class)
-              .withProfile(profileUri).execute();
-    } catch (Exception e) {
-      Util.traceFromException(e);
-      logger.warn("listAllOrganizations: exception: " + Util.traceFromException(e));
-      return null;
-    }
-  }
-
-  /**
-   * Extracts a list of {@link Organization} resources from a given FHIR {@link Bundle}.
-   *
-   * <p>This method filters the entries in the bundle, ensuring they are of type {@link Organization}
-   * and that they conform to the specified profile URL. The extracted organizations are returned
-   * as a list.
-   *
-   * @param bundle The FHIR {@link Bundle} containing organization resources.
-   * @param profileUrl The expected profile URL to filter organizations.
-   * @return A list of {@link Organization} objects matching the specified profile.
-   */
-  protected static List<Organization> extractOrganizations(Bundle bundle, String profileUrl) {
-    return bundle.getEntry().stream()
-        .map(BundleEntryComponent::getResource)
-        .filter(r -> r.getResourceType() == ResourceType.Organization)
-        .filter(r -> r.getMeta().hasProfile(profileUrl))
-        .map(r -> (Organization) r)
-        .collect(Collectors.toList());
-  }
-
-  /**
-   * Fetches specimens from the FHIR server and groups them by their collection id.
-   * If no default collection id is provided, tries to find one from the available collections.
-   * If the default collection id is invalid or not found, removes the specimens without a collection id from the result.
-   *
-   * @param defaultBbmriEricCollectionId the default collection id supplied by the site, to be used for specimens without a collection id. May be null
-   * @return a map of collection id to list of specimens, or null in case of an error
-   */
-  public Map<String,List<Specimen>> fetchSpecimensByCollection(BbmriEricId defaultBbmriEricCollectionId) {
-    logger.debug("fetchSpecimensByCollection: entered");
-
-    // This method is slow, so use cached value if available.
-    if (specimensByCollection != null)
-      return specimensByCollection;
-
-    logger.debug("fetchSpecimensByCollection: get specimens from FHIR store");
-
-    try {
-      specimensByCollection = getAllSpecimensAsMap();
-
-      logger.info("fetchSpecimensByCollection: TTTTTTTTTTTTTTTTTTTTTTTTTTT specimensByCollection size: " + specimensByCollection.size());
-
-      defaultBbmriEricCollectionId = determineDefaultCollectionId(defaultBbmriEricCollectionId, specimensByCollection);
-
-      logger.info("fetchSpecimensByCollection: TTTTTTTTTTTTTTTTTTTTTTTTTTT defaultBbmriEricCollectionId: " + defaultBbmriEricCollectionId);
-
-      // Remove specimens without a collection from specimensByCollection, but keep
-      // the relevant specimen list, just in case we have a valid default ID to
-      // associate with them.
-      List<Specimen> defaultCollection = specimensByCollection.remove(DEFAULT_COLLECTION_ID);
-
-      if (defaultCollection == null)
-        logger.info("fetchSpecimensByCollection: defaultCollection is null");
-      else
-        logger.info("fetchSpecimensByCollection: TTTTTTTTTTTTTTTTTTTTTTTTTTT defaultCollection size: " + defaultCollection.size());
-
-      // Replace the DEFAULT_COLLECTION_ID key in specimensByCollection by a sensible collection ID,
-      // assuming, of course, that there were any specemins caregorized by DEFAULT_COLLECTION_ID.
-      if (defaultCollection != null && defaultCollection.size() != 0 && defaultBbmriEricCollectionId != null) {
-        logger.info("fetchSpecimensByCollection: TTTTTTTTTTTTTTTTTTTTTTTTTTT Replace the DEFAULT_COLLECTION_ID key");
-
-        if (specimensByCollection.containsKey(defaultBbmriEricCollectionId.toString()))
-          // Add all specimens with DEFAULT_COLLECTION_ID to defaultBbmriEricCollectionId if it exists
-          for (Specimen specimen : defaultCollection)
-            specimensByCollection.get(defaultBbmriEricCollectionId.toString()).add(specimen);
-        else
-          // Move all specimens with DEFAULT_COLLECTION_ID to defaultBbmriEricCollectionId
-          specimensByCollection.put(defaultBbmriEricCollectionId.toString(), defaultCollection);
-      }
-
-      logger.info("fetchSpecimensByCollection: TTTTTTTTTTTTTTTTTTTTTTTTTTT specimensByCollection size: " + specimensByCollection.size());
-      if (specimensByCollection.size() == 0)
-        logger.warn("fetchSpecimensByCollection: no collections found, maybe you need to upload some data to your FHIR store?");
-      else {
-        String randomCollectionId = (String) specimensByCollection.keySet().toArray()[0];
-        logger.info("fetchSpecimensByCollection: TTTTTTTTTTTTTTTTTTTTTTTTTTT for collection ID " + randomCollectionId + ", specimen count is: " + specimensByCollection.get(randomCollectionId).size());
-      }
-      return specimensByCollection;
-    } catch (Exception e) {
-      logger.warn("fetchSpecimensByCollection: exception" + Util.traceFromException(e));
-      return null;
-    }
+  protected String getCollectionProfileUri() {
+    return COLLECTION_PROFILE_URI;
   }
 
   public int calculateTotalSpecimenCount(BbmriEricId defaultBbmriEricCollectionId) {
@@ -623,48 +417,6 @@ public class FhirApiBbmriDe extends FhirApi {
   }
 
   /**
-     * Determines a plausible collection id for specimens that do not have a collection id.
-     * If no default collection id is provided, tries to find one from the available collections.
-     * If no valid collection id can be found, returns null.
-     *
-     * @param defaultBbmriEricCollectionId the default collection id supplied by the site
-     * @param specimensByCollection a map of collection id to list of specimens
-     * @return the default collection id, or null if none is found
-     */
-  protected BbmriEricId determineDefaultCollectionId(BbmriEricId defaultBbmriEricCollectionId, Map<String,List<Specimen>> specimensByCollection) {
-    logger.debug("determineDefaultCollectionId: entered");
-    logger.debug("determineDefaultCollectionId: initial defaultBbmriEricCollectionId: " + defaultBbmriEricCollectionId);
-
-    // If no default collection ID has been provided by the site, see if we can find a plausible value.
-    // If there are no specimens with a collection ID, but there is a single collection,
-    // then we can reasonably assume that the collection can be used as a default.
-    if (defaultBbmriEricCollectionId == null && specimensByCollection.size() == 1 && specimensByCollection.containsKey(DEFAULT_COLLECTION_ID)) {
-      logger.debug("determineDefaultCollectionId: first conditional succeeded");
-
-      List<Organization> collections = listAllCollections();
-      if (collections != null) {
-        logger.debug("determineDefaultCollectionId: second conditional succeeded");
-
-        if (collections.size() == 1) {
-          logger.debug("determineDefaultCollectionId: third conditional succeeded");
-
-          String defaultCollectionId = extractValidDirectoryIdentifierFromCollection(collections.get(0));
-
-          logger.debug("determineDefaultCollectionId: defaultCollectionId: " + defaultCollectionId);
-
-          defaultBbmriEricCollectionId = BbmriEricId
-          .valueOf(defaultCollectionId)
-          .orElse(null);
-        }
-      }
-    }
-
-    logger.debug("determineDefaultCollectionId: final defaultBbmriEricCollectionId: " + defaultBbmriEricCollectionId);
-
-    return defaultBbmriEricCollectionId;
-  }
-
-  /**
    * Extracts the collection id from a reference to a collection ID obtained from an extension to Specimen.
    *
    * @param reference
@@ -773,28 +525,6 @@ public class FhirApiBbmriDe extends FhirApi {
     }
 
     return results;
-  }
-
-  /**
-   * Gets the Directory collection ID from the identifier of the supplied collection.
-   * Returns DEFAULT_COLLECTION_ID if there is no identifier or if the identifier's value is not a valid
-   * Directory ID.
-   *
-   * @param collection
-   * @return
-   */
-  protected String extractValidDirectoryIdentifierFromCollection(Organization collection) {
-    String collectionId = DEFAULT_COLLECTION_ID;
-    List<Identifier> collectionIdentifiers = collection.getIdentifier();
-    for (Identifier collectionIdentifier : collectionIdentifiers) {
-      String collectionIdentifierString = collectionIdentifier.getValue();
-      if (BbmriEricId.isValidDirectoryCollectionIdentifier(collectionIdentifierString)) {
-        collectionId = collectionIdentifierString;
-        break;
-      }
-    }
-
-    return collectionId;
   }
 
   /**
